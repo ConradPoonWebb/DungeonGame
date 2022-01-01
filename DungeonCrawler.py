1a6b9d1253247fffe503f113ef294b075bc6ded9
ConradPoonWebb <76155770+ConradPoonWebb@users.noreply.github.com>
2022-01-01 16:46:45 -0700

    Finished Branch Function 1/1/22
    (Hopefully) Finished with branch functions to create branches. To create branches, though, functions outside the class will be required so I will probably have to code that into the game part of the program. I also added a checkPath function since it's the easiest way to check a list of coordinates without having to make sure that the original function outputs some proper coordinates.

--- DungeonCrawler.py
+++ DungeonCrawler.py
@@ -20,7 +20,7 @@
 
 """ACKNOWLEDGEMENTS:
    Base code courtesy of Harvey Mudd, from their Connect 4 homework project
-   Creators of the random and time modules for python. Thanks a ton."""
+   Creators of the random, math, and time modules for python. Thanks a ton."""
 
 """Notes:
    Blue - 34
@@ -114,6 +114,14 @@ class Dungeon:
 
         return dist
     
+    def distFromPoint(self,posX1,posX2,posY1,posY2):
+        """Find distance between two points"""
+        xDiff = abs(posX2 - posX1)
+        yDiff = abs(posY2 - posY1)
+        dist = sqrt(xDiff + yDiff)
+
+        return dist
+    
     def enterToExit(self):
         """Create a path from the entrance
            to the exit
@@ -173,12 +181,26 @@ class Dungeon:
         if (self.checkEmpty(posX,posY-1) == False): #Check Up
             count += 1
         return count
+    
+    def neighborRoomsPos(self,posX,posY):
+        """Check where the neighboring
+           rooms occupy"""
+        direction = ['R','L','D','U']
+        if (self.checkEmpty(posX+1,posY) == False): #Check Right
+            direction.remove('R')
+        if (self.checkEmpty(posX-1,posY) == False): #Check Left
+            direction.remove('L')
+        if (self.checkEmpty(posX,posY+1) == False): #Check Down
+            direction.remove('D')
+        if (self.checkEmpty(posX,posY-1) == False): #Check Up
+            direction.remove('U')
+        return direction
 
     def checkBranch(self,posX,posY):
         """Check the availability of
            a branch"""
         coords = []
-        if self.neighborRooms(posX,posY) == 2 and (self.checkEmpty(posX,posY) == False):
+        if self.neighborRooms(posX,posY) <= 2 and (self.checkEmpty(posX,posY) == False):
             posPlaces = []
             if self.checkEmpty(posX+1,posY): #Check Right
                 posPlaces.append((posX+1,posY))
@@ -201,6 +223,8 @@ class Dungeon:
             return False
     
     def branchCoords(self,posX,posY):
+        """Find the coords of a branching
+           coordinate"""
         posPlaces = []
         coords = []
         if self.checkEmpty(posX+1,posY): #Check Right
@@ -218,18 +242,121 @@ class Dungeon:
                 coords.append(coord)
         
         return coords
+    
+    def makeBranchEnd(self,posX,posY):
+        """Branches should be roughly 2-5 units away
+           from the starting coord"""
+        maxDist = random.choice(list(range(2,6)))
+        posPoints = []
+        for row in range(0,self.fY): #Finding points
+            for col in range(0,self.fX):
+                if (row == posY) and (col == posX):
+                    continue
+                if self.distFromPoint(posX,col,posY,row) <= maxDist:
+                    if not(self.distFromPoint(posX,col,posY,row) < 2):
+                        posPoints.append((col,row))
         
+        posDirect = self.neighborRoomsPos(posX,posY)
 
+        for coord in posPoints: #Filter points
+            if 'R' not in posDirect:
+                if coord[0] > posX:
+                    posPoints.remove(coord)
+                    continue
+            if 'L' not in posDirect:
+                if coord[0] < posX:
+                    posPoints.remove(coord)
+                    continue
+            if 'D' not in posDirect:
+                if coord[1] > posY:
+                    posPoints.remove(coord)
+                    continue
+            if 'U' not in posDirect:
+                if coord[1] < posY:
+                    posPoints.remove(coord)
+                    continue
+            if self.neighborRooms(coord[0],coord[1]) > 1:
+                posPoints.remove(coord)
+                continue
+            if self.checkEmpty(coord[0],coord[1]) == False:
+                posPoints.remove(coord)
+                continue
+        
+        c = random.choice(posPoints)
+        while self.neighborRooms(c[0],c[1]) > 1:
+            c = random.choice(posPoints)
+
+        return c
+
+    def pointToPoint(self,posX1,posY1,posX2,posY2):
+        """Make a randomized path between 
+           two points
+           Add first point separately"""
+        currentX,currentY = posX1,posY1
+        path = []
+
+        while self.distFromPoint(currentX,posX2,currentY,posY2) != 1.0:
+            moveList = []
+            moveDict = {}
+
+            if self.checkEmpty(currentX+1,currentY): #Check Right
+                moveList.append((currentX+1,currentY))
+            if self.checkEmpty(currentX-1,currentY): #Check Left
+                moveList.append((currentX-1,currentY))
+            if self.checkEmpty(currentX,currentY+1): #Check Down
+                moveList.append((currentX,currentY+1))
+            if self.checkEmpty(currentX,currentY-1): #Check Up
+                moveList.append((currentX,currentY-1))
+            
+            for coord in moveList:
+                moveDict[coord] = self.distFromPoint(coord[0],posX2,coord[1],posY2)
+            
+            sort_moveDict = sorted(moveDict.items(), key=lambda x: x[1])
+            
+            if len(sort_moveDict) > 1:
+                if (sort_moveDict[0][1] == sort_moveDict[1][1]) and (sort_moveDict[0][0] not in path):
+                    closestCoord = sort_moveDict[random.choice([0,1])][0]
+                elif sort_moveDict[0][0] in path:
+                    closestCoord = sort_moveDict[1][0]
+                else:
+                    closestCoord = sort_moveDict[0][0]
+            else:
+                closestCoord = sort_moveDict[0][0]
+
+            currentX,currentY = closestCoord[0],closestCoord[1]
+            path.append((currentX,currentY))
+        
+        path.append((posX2,posY2))
+        
+        return path
+    
+    def checkPath(self,path):
+        for coord in path:
+            if self.neighborRooms(coord[0],coord[1]) >= 2:
+                return False
+        
+        return True
 
 
 #tests
 d = Dungeon(5,5)
 d.createEntrance(0,0)
-d.createExit(4,4)
+d.createExit(0,4)
 path = d.enterToExit()
 d.createManyRooms(path)
-if d.checkBranch(1,0):
-    print(d.branchCoords(1,0))
+#if d.checkBranch(1,0):
+    #print(d.branchCoords(1,0))
+    #print(d.makeBranchCoord(1,0))
 if d.checkBranch(0,1):
-    print(d.branchCoords(0,1))
+    b = d.branchCoords(0,1)[0]
+    print(b)
+    bE = d.makeBranchEnd(0,1)
+    while d.neighborRooms(bE[0],bE[1]) != 0: #REQUIRED IN FINAL CODE!!! This is the only way that works for some reason
+        bE = d.makeBranchEnd(0,1)
+    print(bE)
+    d.createRoom(b[0],b[1],'?')
+    p = d.pointToPoint(b[0],b[1],bE[0],bE[1])
+    while d.checkPath(p) == False:
+        p = d.pointToPoint(b[0],b[1],bE[0],bE[1])
+    d.createManyRooms(p)
 print(d)
